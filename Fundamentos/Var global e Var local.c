d8a2217db57c0b2d5b27dbfae27e57260b82a075
Guh90 <57641874+Guh90@users.noreply.github.com>
2025-07-07 08:09:41 -0400

    feat 1º programa (dog)

---
+++ Fundamentos/Var global e Var local.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int x_global=10;
+int main()
+{
+ int x_local=5;
+  printf (" Variável global: %d\n Variável local: %d\n", x_global, x_local);
+  //ou dois printf, 1 pra cada variável.
+  return 0;
+}
